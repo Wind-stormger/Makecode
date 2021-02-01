957c3de37285f28bec288f94d9fb6323b8bd27ba
Wind-stormger <76462385+Wind-stormger@users.noreply.github.com>
2021-02-01 10:27:37 +0800

    none

---
+++ Thinking in MakeCode/1.1blocking_test/main.ts
@@ -0,0 +1,13 @@
+while (true) {
+    basic.pause(2000)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(2000)
+    basic.showIcon(IconNames.Heart)
+    basic.showString("12")
+    basic.pause(2000)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(2000)
+    basic.showIcon(IconNames.Heart)
+    basic.showString("123")
+    basic.showString("ABC")
+}
